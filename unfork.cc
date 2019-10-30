b5cb9f6701db49e4552cfd7ba83a79a400133f27
whitequark <whitequark@whitequark.org>
2019-10-30 05:30:24 +0000

    Return properly aligned pointers from static heap malloc.

--- unfork.cc
+++ unfork.cc
@@ -31,6 +31,7 @@ alignas(alignof(max_align_t)) char heap[0x8000], *heap_end = &heap[0];
 
 void *malloc(size_t size) {
   void *block = heap_end;
+  size = (size + alignof(max_align_t) - 1) & ~(alignof(max_align_t) - 1);
   if ((ssize_t)size > &heap[sizeof(heap)] - heap_end) {
     // Print a message even if the caller can handle allocation failure, since in some cases it
     // may cause a hard to understand failure downstream (like fscanf only parsing some of
